930461d817464dbb698cdc040a60c5dfc3b6375d
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-10-21 00:25:51 -0400

    Now parsing egyptian braces and displaying line numbers

--- CSharpStyleChecker/CSharpStyleChecker/__init__.py
+++ CSharpStyleChecker/CSharpStyleChecker/__init__.py
@@ -6,7 +6,7 @@ base_dir = os.path.split(__file__)[0]
 
 regex_egyptianbrace = re.compile(r'\w+.*\{\s*')
 
-def styleCheck(fileText):
+def styleCheck(fileContents, fileName):
    """
    Checks the content of fileName
    to see if it is 'valid' C# as
@@ -14,10 +14,13 @@ def styleCheck(fileText):
    standards goes
    """
 
-   regexMsg = regex_egyptianbrace.findall(fileText)
+   regexMsg = regex_egyptianbrace.findall(fileContents)
    if regexMsg:
        for line in regexMsg:
-           print "Error 001! ", line
+           with open(fileName, 'r') as f:
+               for num, text in enumerate(f):
+		   if line.rstrip('\n ') in text:
+                       print "Error 001 on line: ", (num + 1), " ", line
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
@@ -26,5 +29,5 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     with open(os.path.join(base_dir, args.file[0]), "r") as codeFile:
-	styleCheck(codeFile.read())
+	styleCheck(codeFile.read(), args.file[0])
 
